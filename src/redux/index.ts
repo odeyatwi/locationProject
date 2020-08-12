5889cc4216806c9d3a7686c80165c5f6af09b9c1
odeya <odeyatwi@gmail.com>
2020-08-13 00:42:46 +0300

    add async error handler

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,8 +1,9 @@
 import {applyMiddleware, createStore} from "redux";
 import thunk from "redux-thunk";
 import reducers from "./reducers";
+import crashReporter from "./ErrorHandler";
 
-const store = createStore(reducers,{},applyMiddleware(thunk))
+const store = createStore(reducers,{},applyMiddleware(crashReporter,thunk))
 
 export default store
 
